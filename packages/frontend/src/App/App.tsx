6ab53604b2626912ef67ef08139f5c4c32a29b85
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2021-08-24 12:25:19 +0700

    Update App.tsx

--- packages/frontend/src/App/App.tsx
+++ packages/frontend/src/App/App.tsx
@@ -40,7 +40,7 @@ export const App = (): JSX.Element => {
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<h1 className="App-title">
-					Welcome to Login with MetaMask
+					Welcome to Login with MetaMask Demo
 				</h1>
 			</header>
 			<div className="App-intro">
